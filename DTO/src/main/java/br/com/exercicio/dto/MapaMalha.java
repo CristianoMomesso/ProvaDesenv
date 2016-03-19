534e5bdbfc739101d60fbaadb4ebe7eeb090ad5a
Cristiano <momesso173@gmail.com>
2016-03-19 12:30:01 -0300

    javadoc

--- DTO/src/main/java/br/com/exercicio/dto/MapaMalha.java
+++ DTO/src/main/java/br/com/exercicio/dto/MapaMalha.java
@@ -2,24 +2,47 @@ package br.com.exercicio.dto;
 
 import java.util.List;
 
+/**
+ * Clase de entrada para o mapa da malha 
+ * @author Cristiano Momesso
+ *
+ */
 public class MapaMalha {
 
+	/**
+	 * Atributo nomeMapa do tipo String
+	 */
 	private String nomeMapa;
 	
+	/**
+	 * Atributo rotas do tipo List<RotaMalha>
+	 */
 	private List<RotaMalha> rotas;
 
+	/**
+	 * @return nomeMapa
+	 */
 	public String getNomeMapa() {
 		return nomeMapa;
 	}
 
+	/**
+	 * @param nomeMapa
+	 */
 	public void setNomeMapa(String nomeMapa) {
 		this.nomeMapa = nomeMapa;
 	}
 
+	/**
+	 * @return rotas
+	 */
 	public List<RotaMalha> getRotas() {
 		return rotas;
 	}
 
+	/**
+	 * @param rotas
+	 */
 	public void setRotas(List<RotaMalha> rotas) {
 		this.rotas = rotas;
 	}
--- DTO/src/main/java/br/com/exercicio/dto/RotaMalha.java
+++ DTO/src/main/java/br/com/exercicio/dto/RotaMalha.java
@@ -1,25 +1,56 @@
 package br.com.exercicio.dto;
 
+/**
+ * @author Cristiano Momesso
+ *
+ */
 public class RotaMalha {
+	/**
+	 * Atributo p1 do tipo String
+	 */
 	private String p1;
+	/**
+	 * Atributo p2 do tipo String
+	 */
 	private String p2;
+    /**
+     * Atributo distancia do tipo double
+     */
     private double distancia;
     
+	/**
+	 * @return p1
+	 */
 	public String getP1() {
 		return p1;
 	}
+	/**
+	 * @param p1
+	 */
 	public void setP1(String p1) {
 		this.p1 = p1;
 	}
+	/**
+	 * @return p2
+	 */
 	public String getP2() {
 		return p2;
 	}
+	/**
+	 * @param p2
+	 */
 	public void setP2(String p2) {
 		this.p2 = p2;
 	}
+	/**
+	 * @return distancia
+	 */
 	public double getDistancia() {
 		return distancia;
 	}
+	/**
+	 * @param distancia
+	 */
 	public void setDistancia(double distancia) {
 		this.distancia = distancia;
 	}	
